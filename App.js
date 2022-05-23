a2111c6d9304f1f54eab5f748697f61dd08a7983
murilocostazero <murilocostazero@gmail.com>
2022-05-23 17:00:34 -0300

    Adicionando StatusBar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {View, StatusBar} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {Login, Home} from './src/pages';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SnackBar} from './src/components';
+import colors from './src/styles/colors.style';
 
 const Stack = createNativeStackNavigator();
 
@@ -32,42 +33,49 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      {user ? (
-        <Stack.Navigator>
-          <Stack.Screen name="Home" options={{headerShown: false}}>
-            {props => (
-              <Home
-                onAuthStateChanged={loggedUser =>
-                  onAuthStateChanged(loggedUser)
-                }
-                {...props}
-              />
-            )}
-          </Stack.Screen>
-        </Stack.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" options={{headerShown: false}}>
-            {props => (
-              <>
-                <Login
-                  handleSnackbar={snackbar => handleSnackbar(snackbar)}
+    <>
+      <StatusBar
+        animated={true}
+        backgroundColor={colors.primary}
+        barStyle='light-content'
+      />
+      <NavigationContainer>
+        {user ? (
+          <Stack.Navigator>
+            <Stack.Screen name="Home" options={{headerShown: false}}>
+              {props => (
+                <Home
                   onAuthStateChanged={loggedUser =>
                     onAuthStateChanged(loggedUser)
                   }
                   {...props}
                 />
-                {isSnackbarVisible ? (
-                  <SnackBar snackbar={snackbar} />
-                ) : (
-                  <View />
-                )}
-              </>
-            )}
-          </Stack.Screen>
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+              )}
+            </Stack.Screen>
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen name="Login" options={{headerShown: false}}>
+              {props => (
+                <>
+                  <Login
+                    handleSnackbar={snackbar => handleSnackbar(snackbar)}
+                    onAuthStateChanged={loggedUser =>
+                      onAuthStateChanged(loggedUser)
+                    }
+                    {...props}
+                  />
+                  {isSnackbarVisible ? (
+                    <SnackBar snackbar={snackbar} />
+                  ) : (
+                    <View />
+                  )}
+                </>
+              )}
+            </Stack.Screen>
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </>
   );
 }
