3ed83937de4ce241f0b7ead2ddb693640f615f36
murilocostazero <murilocostazero@gmail.com>
2022-07-24 09:53:40 -0300

    Centralização dos componentes do Scrollview da busca de dispositivos

--- src/pages/SearchPage/styles.style.js
+++ src/pages/SearchPage/styles.style.js
@@ -1,36 +1,39 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 import colors from '../../styles/colors.style';
 
 const styles = StyleSheet.create({
-    deviceNotFoundContainer: {
-        alignItems: 'center'
-    },
-    deviceInfoContainer: {
-        padding: 8,
-        marginTop: 16
-    },
-    alertContainer: {
-        padding: 8,
-        borderRadius: 16
-    }, 
-    alertMessage: {
-        maxWidth: '86%',
-        fontFamily: 'JosefinSans-Medium',
-        color: '#FFF',
-        fontSize: 16,
-        marginLeft: 8
-    },
-    lightLabel: {
-        color: '#FFF',
-        fontFamily: 'JosefinSans-Bold',
-        fontSize: 16,
-        marginLeft: 8
-    },
-    darkLabel: {
-        color: colors.text.dark,
-        fontFamily: 'JosefinSans-Bold',
-        fontSize: 14,
-    }
+  deviceNotFoundContainer: {
+    alignItems: 'center',
+  },
+  deviceFoundContainer: {
+    flex: 1
+  },
+  deviceInfoContainer: {
+    padding: 8,
+    marginTop: 16,
+  },
+  alertContainer: {
+    padding: 8,
+    borderRadius: 16,
+  },
+  alertMessage: {
+    maxWidth: '86%',
+    fontFamily: 'JosefinSans-Medium',
+    color: '#FFF',
+    fontSize: 16,
+    marginLeft: 8,
+  },
+  lightLabel: {
+    color: '#FFF',
+    fontFamily: 'JosefinSans-Bold',
+    fontSize: 16,
+    marginLeft: 8,
+  },
+  darkLabel: {
+    color: colors.text.dark,
+    fontFamily: 'JosefinSans-Bold',
+    fontSize: 14,
+  },
 });
 
 export default styles;
